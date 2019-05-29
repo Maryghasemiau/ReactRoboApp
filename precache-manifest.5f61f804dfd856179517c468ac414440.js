6cfc210dfbda9c7e5c6bb5e1ce514bfedf060d8f
Mary Ghasemi <mary.ghasemi.au@gmail.com>
2019-05-29 10:51:12 +1000

    Updates

---
+++ precache-manifest.5f61f804dfd856179517c468ac414440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff90beec48e93e7c35208767337f9d62",
+    "url": "/ReactRoboApp/index.html"
+  },
+  {
+    "revision": "d957ad2f2306b2416b43",
+    "url": "/ReactRoboApp/static/css/2.2ec98f22.chunk.css"
+  },
+  {
+    "revision": "f1affbefac64ee5d8f0f",
+    "url": "/ReactRoboApp/static/css/main.93383f0a.chunk.css"
+  },
+  {
+    "revision": "d957ad2f2306b2416b43",
+    "url": "/ReactRoboApp/static/js/2.9f060845.chunk.js"
+  },
+  {
+    "revision": "f1affbefac64ee5d8f0f",
+    "url": "/ReactRoboApp/static/js/main.385395ed.chunk.js"
+  },
+  {
+    "revision": "69210976d7f5222e2064",
+    "url": "/ReactRoboApp/static/js/runtime~main.adfa840e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ReactRoboApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
